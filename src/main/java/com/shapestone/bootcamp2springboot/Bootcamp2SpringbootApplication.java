08534a7ce343dd36adfc35b5854076f8c47fbe19
Anil <anil.chirumamilla@shapestone.in>
2023-08-01 09:10:55 +0530

    first commit

---
+++ src/main/java/com/shapestone/bootcamp2springboot/Bootcamp2SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.shapestone.bootcamp2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shapestone/bootcamp2springboot/Bootcamp2SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shapestone.bootcamp2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
